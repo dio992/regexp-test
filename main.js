01abbbc92de997f8da4dd8b3ce7ea952e858ca88
dio992 <dio992@naver.com>
2022-11-01 15:05:04 +0900

    Create Project

---
+++ main.js
@@ -0,0 +1,26 @@
+const str = `
+010-1234-5678.
+therlaskatjr@naver.com
+https://www.naver.com
+The quick brown fox jumps over the lazy dog.
+asddddlkjfdaslkfj
+http://localhost:1234
+동해물과백두산이_마르고 닳도록
+`
+
+//const regexp = new RegExp('the', 'gi')
+//const regexp = /the/gi
+//console.log(str.match(regexp))
+//const regexp = /fox/gi
+//console.log(regexp.test(str))
+//let regexp = /fox/gi
+//str = str.replace(regexp, 'AAA')
+//console.log(str)
+
+const h = `  the hello world       !
+
+`
+console.log(
+  h.replace(/\s/g,'')
+  )
+
